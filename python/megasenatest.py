877126c711542f195e3c33ce35ebea89f5178a19
Maurício Vieira <mauricio@mauriciovieira.net>
2011-11-29 08:14:29 -0200

    Fazendo o parsing. Falta persistir no couchdb

--- python/megasenatest.py
+++ python/megasenatest.py
@@ -11,7 +11,12 @@ class MegasenaTest(unittest.TestCase):
 
     def test_data_primeiro_concurso(self):
         '''O primeiro concurso foi em 11/03/1996'''
-        self.assertEqual(self.parser.concursos[0]['Data Sorteio'],'11/03/1996')
+        self.assertEqual(self.parser.concursos[1]['Data Sorteio'],'11/03/1996')
+
+    def test_resultado_concurso_11(self):
+        '''O resultado do concurso 11 foi 15.591.365,07'''
+        res = '15.591.365,07'
+        self.assertEqual(self.parser.concursos[11]['Rateio_Sena'],res)
 
 if __name__ == '__main__':
     unittest.main()
--- python/parsers.py
+++ python/parsers.py
@@ -4,22 +4,40 @@
 from html.parser import HTMLParser
 
 class MegasenaParser(HTMLParser):
-    title = "Resultado da Mega-senaxa"
-    concursos = [{'Concurso': 1, 'Data Sorteio': '11/03/1996'},
-                 {'Concurso': 2, 'Data Sorteio': '18/03/1996'},
-                ]
+    title = "Resultado"
+    concursos = []
+    headers = []
+    in_headers = False 
     def __init__(self,page="../resultados/D_MEGA.HTM"):
-        self.page = open(page, encoding = 'iso-8859-1').read()
+        page = open(page, encoding = 'iso-8859-1').read()
         HTMLParser.__init__(self)
-        self.feed(self.page)
+        self.feed(page)
 
     def handle_starttag(self, tag, attrs):
         self.current_tag = tag
+        if tag == 'tr':
+            self.line = []
+        elif tag == 'th':
+            self.in_headers = True
+
     def handle_endtag(self, tag):
+        if tag == 'tr':
+            conc = dict(zip(self.headers, self.line))
+            self.concursos.append(conc)
+        elif tag == 'th':
+            self.in_headers = False
         self.current_tag = None 
+
     def handle_data(self, data):
         if self.current_tag == 'title':
             self.title = data
+        elif self.current_tag == 'font' and self.in_headers:
+            self.headers.append(data)
+        elif self.current_tag == 'td':
+            self.line.append(data)
+        else:
+            pass
 
 if __name__ == '__main__':
-   mp = MegasenaParser() 
+    mp = MegasenaParser() 
+    print(mp.concursos)
