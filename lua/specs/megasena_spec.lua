2212f7057c4d1648a1d735db8a4e4dcba9fe1cc8
Maurício Vieira <mauricio@mauriciovieira.net>
2011-12-04 22:07:02 -0200

    Parsing em lua

--- lua/specs/megasena_spec.lua
+++ lua/specs/megasena_spec.lua
@@ -3,16 +3,16 @@ require 'src.parser'
 
 describe["Parser da megasena"] = function ()
     it ["A primeira frase deve ser Resultado da Mega-sena"] = function()
-        expect (megasena({title = true})).should_be("Resultados da Mega-sena")
+        expect (megasena()['title']).should_be("Resultado da Mega-sena")
     end
 
     it ["O primeiro concurso foi em 11/03/1996"] = function()
-        expect (megasena({"concursos",1,"Data sorteio"})).should_be("11/03/1996")
+        expect (megasena()["concursos"][1]["Data Sorteio"]).should_be("11/03/1996")
     end
     
     it ["O premio do concurso 11 foi 15.591.365,07"] = function()
         local res = '15.591.365,07'
-        expect( megasena({"concursos",11,"Rateio_Sena"})).should_be(res)
+        expect(megasena()["concursos"][11]["Rateio_Sena"]).should_be(res)
     end
 end
 
--- lua/src/parser.lua
+++ lua/src/parser.lua
@@ -1,17 +1,47 @@
 local html = require 'lib.html'
 
-function megasena(args)
+function dict_zip(t1, t2)
+    local res = {} 
+    assert(table.getn(t1) == table.getn(t2))
+    for i=1,table.getn(t1) do
+        res[t1[i]] = t2[i]
+    end
+    return res
+end
 
+function get_elements(t)
+    local res = {}
+    for i=1,table.getn(t) do
+            local value = ''
+            if type(t[i][1]) == 'string' then
+                value = t[i][1]
+            elseif type(t[i][1][1]) == 'table' then
+                value = t[i][1][1][1]
+            end
+            assert(type(value == 'string'))
+            table.insert(res,value)
+        end 
+        return res
+    end
+
+function megasena()
     io.input('../resultados/D_MEGA.HTM')
     local h = io.read("*all")
-    local parsed = html.collect(h)
+    local raw = html.collect(h)
+    local body = raw[1][2] -- body
+    local main_table = body[3]
 
-    local title = 'Resultados da Mega-sena'
-    if args.title then
-        return title 
-    elseif args[2] == 1 then
-        return "11/03/1996"
-    else 
-        return '15.591.365,07'
+    local headers = get_elements(main_table[1]) --th s
+    local concursos = {}
+    for i=2,table.getn(main_table) do
+        linha = get_elements(main_table[i])
+        concurso = dict_zip(headers,linha)
+        table.insert(concursos,concurso)
     end
+
+    local parsed = {
+        title = body[1][1][1][1][1][1],
+        concursos = concursos,
+    }
+    return parsed
 end
