5d019efb5e082cf22d56be434d75eae111d4ca72
Maurício Vieira <mauricio@mauriciovieira.net>
2011-12-04 22:08:11 -0200

    Parser em python confere os jogos de megasena e lotofácil

--- python/parsers.py
+++ python/parsers.py
@@ -4,12 +4,12 @@
 from html.parser import HTMLParser
 import json 
 
-class MegasenaParser(HTMLParser):
+class LoteriasParser(HTMLParser):
     title = "Resultado"
     concursos = []
     headers = []
     in_headers = False 
-    def __init__(self,page="../resultados/D_MEGA.HTM"):
+    def __init__(self,page):
         page = open(page, encoding = 'iso-8859-1').read()
         HTMLParser.__init__(self)
         self.feed(page)
@@ -42,6 +42,38 @@ class MegasenaParser(HTMLParser):
     def to_json(self):
         return json.dumps(self.concursos)
 
+    def sorteados(self, num):
+        """Apostas sorteadas para o concurso num"""
+        apostas = self.apostas or []
+        concurso = {} if num>=len(self.concursos) else self.concursos[num]
+        return set([concurso.get(x,0) for x in apostas])
+
+    def confere(self, num, apostas):
+        res = []
+        if num >= len(self.concursos):
+            res.append("Concurso %d - não aconteceu" % num )
+        else:
+            res.append("Concurso: %d - %s" % (num, self.concursos[num]['Data Sorteio']))
+            res.append("\tJogos sorteados: %s" % (" ".join(sorted(self.sorteados(num)))))
+            res.append("\tJogos    feitos: %s" % (" ".join(apostas)))
+            acertos = self.sorteados(num).intersection(apostas)
+            res.append("\t%d acertos - %s" % (len(acertos), " ".join(sorted(acertos))))
+        return "\n".join(res)
+
+class MegasenaParser(LoteriasParser):
+    apostas = [ x for x in map(lambda s: "%dª Dezena" %s, range(1,7)) ]
+    def __init__(self,page="../resultados/D_MEGA.HTM"):
+        LoteriasParser.__init__(self,page)
+
+class LotofacilParser(LoteriasParser):
+    apostas = [ x for x in map(lambda s: "Bola%d" % s, range(1,16)) ]
+    def __init__(self,page="../resultados/D_LOTFAC.HTM"):
+        LoteriasParser.__init__(self,page)
+
 if __name__ == '__main__':
     mp = MegasenaParser() 
-    print(mp.to_json())
+    from pprint import pprint 
+    pprint(mp.concursos)
+    lp = LotofacilParser()
+    pprint(lp.concursos)
+
