41b36b57455feeb2bd388cf80cf1205de587f159
Maurício Vieira <mauricio@mauriciovieira.net>
2011-12-04 22:10:25 -0200

    Download da Lotofácil

--- resultados/download.sh
+++ resultados/download.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 wget http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip && unzip D_megase.zip
+wget http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip && unzip D_lotfac.zip 
