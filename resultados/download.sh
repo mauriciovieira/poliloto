f95080ab43ecbfa7739e6101c221df2a9cd000d4
Maurício Vieira <mauricio@mauriciovieira.net>
2011-12-07 09:37:42 -0200

    Download de todos os resultados

--- resultados/download.sh
+++ resultados/download.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-wget http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip && unzip D_megase.zip
-wget http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip && unzip D_lotfac.zip 
+zips=( "D_megase.zip" "D_quina.zip" "d_dplsen.zip" "D_federa.zip" "d_loteca.zip" "D_timema.zip" "D_lotfac.zip" "D_lotoma.zip" "d_lotogo.zip" )
+base_url=http://www1.caixa.gov.br/loterias/_arquivos/loterias
+
+for zip in "${zips[@]}"
+do
+	wget "${base_url}/$zip" && unzip -o $zip &&	rm $zip
+done
